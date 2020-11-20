eb9f1550b7c68a82d5b9c57807f1a8ba40202943
Miller Meares <benmillermeares3@gmail.com>
2020-11-20 13:56:11 -0500

    init commit

---
+++ DeliveryServiceRecognition/GetShippingCourier.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DeliveryServiceRecognition
+{
+    class GetShippingCourier
+    {
+        public static string GetCourierFromNumber(string trackingNumber)
+        {
+            trackingNumber = trackingNumber.Replace(" ", "");
+
+            string courier = "Unknown";
+            if (IsFedExTrack(trackingNumber))
+            {
+                courier = "FedEx";
+            }
+            if (IsUSPSTrack(trackingNumber))
+            {
+                courier = "USPS";
+            }
+            if (IsUPSTrack(trackingNumber))
+            {
+                courier = "UPS";
+            }
+            if (IsFedExUPS_Overlap(trackingNumber))
+            {
+                courier = "UPS/FedEx";
+            }
+
+
+            return courier;
+        }
+        // regex largely derived from this post. Some modifications made in testing: https://andrewkurochkin.com/blog/code-for-recognizing-delivery-company-by-track
+        private static bool IsUPSTrack(string trackingNumber)
+        {
+            List<Regex> validators = new List<Regex>();
+            validators.Add(new Regex(@"^(1Z)[0-9A-Z]{16}$"));
+            validators.Add(new Regex(@"^(T)+[0-9A-Z]{10}$"));
+            validators.Add(new Regex(@"^[0-9]{9}$"));
+            validators.Add(new Regex(@"^[0-9]{26}$"));
+
+            foreach (Regex val in validators)
+            {
+                if (val.IsMatch(trackingNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //source for regular expressions: https://andrewkurochkin.com/blog/code-for-recognizing-delivery-company-by-track
+        private static bool IsFedExTrack(string trackingNumber)
+        {
+            List<Regex> validators = new List<Regex>();
+            validators.Add(new Regex(@"^[0-9]{20}$"));
+            validators.Add(new Regex(@"^[0-9]{15}$"));
+            validators.Add(new Regex(@"^[0-9]{22}$"));
+
+            foreach (Regex val in validators)
+            {
+                if (val.IsMatch(trackingNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //source for regular expressions: https://andrewkurochkin.com/blog/code-for-recognizing-delivery-company-by-track
+        private static bool IsUSPSTrack(string trackingNumber)
+        {
+            List<Regex> validators = new List<Regex>();
+            validators.Add(new Regex(@"^(94|93|92|94|95)[0-9]{20}$"));
+            validators.Add(new Regex(@"^(94|93|92|94|95)[0-9]{22}$"));
+            validators.Add(new Regex(@"^(70|14|23|03)[0-9]{14}$"));
+            validators.Add(new Regex(@"^(M0|82)[0-9]{8}$"));
+            validators.Add(new Regex(@"^([A-Z]{2})[0-9]{9}([A-Z]{2})$"));
+
+            foreach (Regex val in validators)
+            {
+                if (val.IsMatch(trackingNumber))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static bool IsFedExUPS_Overlap(string trackingNumber)
+        {
+            return new Regex(@"^[0-9]{12}$").IsMatch(trackingNumber);
+        }
+    }
+}
---
+++ DeliveryServiceRecognition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DeliveryServiceRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _1 = "9999 9999 9999";
+            string _2 = "9999 9999 9999 999";
+            string _3 = "9999 9999 9999 9999 9999";
+            string _4 = "9999 9999 9999 9999 9999 99";
+
+            string _5 = "1Z 9999 9999 9999 9999";
+            string _6 = "9999 9999 9999";
+            string _7 = "T 9999 9999 99";
+            string _8 = "9999 99999";
+
+            string _9 = "9400 1000 0000 0000 0000 00";
+            string _10 = "9205 5000 0000 0000 0000 00";
+            string _11 = "9407 3000 0000 0000 0000 00";
+            string _12 = "9407 3000 0000 0000 0000 00";
+
+            string[] numbers = new string[] { _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12 };
+            foreach (string num in numbers)
+            {
+                Console.WriteLine(GetShippingCourier.GetCourierFromNumber(num));
+            }
+        }
+
+    }
+}
